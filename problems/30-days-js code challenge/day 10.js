141d40dad6ce6c98c857dc75c3446bcba2d61cae
Yabe12 <yeabsirabehailu92@gmail.com>
2024-08-13 23:40:09 +0300

    up to day 10 complet

---
+++ problems/30-days-js code challenge/day 10.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+function compose(functions) {
+    return function(x) {
+    
+        return functions.reduceRight((acc, fn) => fn(acc), x);
+    };
+}
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
