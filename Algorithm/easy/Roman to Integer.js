1735322fe875bc80003b37fc9c2d1a0e20dcd5cc
Yabe12 <yeabsirabehailu92@gmail.com>
2024-11-18 22:10:26 +0300

    the roman convert to intger have in two method

--- Algorithm/easy/Roman to Integer.js
+++ Algorithm/easy/Roman to Integer.js
@@ -25,3 +25,29 @@ var romanToInt = function (s) {
 
   return x;
 };
+//  /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+// var romanToInt = function (s) {
+//   let x = 0;
+//   const num = {
+//       'I': 1,
+//       'V': 5,
+//       'X': 10,
+//       'L': 50,
+//       'C': 100,
+//       'D': 500,
+//       'M': 1000
+//   }
+//   for (let i = 0; i <= s.length-1; i++) {
+//       if (num[s[i]] < num[s[i + 1]]) {
+//           x -= num[s[i]]
+//       }
+//       else {
+//           x += num[s[i]]
+//       }
+//   }
+
+//   return x;
+// };
