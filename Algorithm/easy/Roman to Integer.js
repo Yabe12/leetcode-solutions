55d6fd7db0d9271da131ea69e30c0ff8a2bf079f
Yabe12 <yeabsirabehailu92@gmail.com>
2024-11-18 22:08:54 +0300

    roman number converter

---
+++ Algorithm/easy/Roman to Integer.js
@@ -0,0 +1,27 @@
+// 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  let x = 0;
+  const num = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+  }
+  for (let i = 0; i <= s.length-1; i++) {
+      if (num[s[i]] < num[s[i + 1]]) {
+          x -= num[s[i]]
+      }
+      else {
+          x += num[s[i]]
+      }
+  }
+
+  return x;
+};
